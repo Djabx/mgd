d48b4f0520afa7978910cb32cf68cdebb64a89d4
Alexandre Badez <alexandre.badez@gmail.com>
2014-05-18 23:51:46 +0200

    Improve storage module

--- src/mgd/store.py
+++ src/mgd/store.py
@@ -4,22 +4,100 @@
 
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, LargeBinary
+from contextlib import contextmanager
+from sqlalchemy import Column, ForeignKey, Integer, String, LargeBinary, UniqueConstraint, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base()
 
+__DB = None
+__SESSION_MAKER = None
+
+def create_db(file_name='mdg.store', force=False):
+  global __DB
+  if __DB is not None and force is False:
+    # TODO: create some real exceptions
+    raise Exception('Engine already exist !')
+  db = __DB = create_engine('sqlite:///' + file_name)
+  # Create all tables in the engine. This is equivalent to "Create Table"
+  # statements in raw SQL.
+  Base.metadata.create_all(db)
+  return db
+
+
+def get_db():
+  return __DB
+
+
+def get_session():
+  global __SESSION_MAKER
+  db = get_db()
+  if __SESSION_MAKER is None:
+    sm =__SESSION_MAKER = sessionmaker()
+    sm.configure(bind=db)
+
+  return __SESSION_MAKER()
+
+
+def remove_db():
+  global __DB, __SESSION_MAKER
+  __DB = None
+  __SESSION_MAKER = None
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = get_session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def run_my_program():
+    with session_scope() as session:
+        ThingOne().go(session)
+        ThingTwo().go(session)
+
+
+
+################################################################################
+# Model
+################################################################################
 class Site(Base):
   __tablename__ = 'site'
   id = Column(Integer, primary_key=True)
   name = Column(String(250))
   hostname = Column(String(250))
+
   mangas = relationship(
         'Manga',
         secondary='site_manga_link'
   )
+
+  @classmethod
+  def find_with_hostname(cls, hn):
+    with session_scope() as s:
+      result = s.query(Site).filter(Site.hostname==hn).all()
+      result_l = len(result)
+      if result_l > 1:
+        # too much result
+        return None
+      elif result_l == 1:
+        # ok return
+        return result[0]
+      else:
+        # no result
+        return None
 
 
 class Manga(Base):
@@ -40,7 +118,7 @@ class SiteMangaLink(Base):
 
 class Chapter(Base):
   __tablename__ = 'chapter'
-__table_args__ = (
+  __table_args__ = (
       UniqueConstraint('site_id', 'manga_id', 'num'),
   )
   id = Column(Integer, primary_key=True)
@@ -52,8 +130,8 @@ __table_args__ = (
   done = Column(Boolean()) # if we download the chapter or not
 
   manga = relationship(Manga)
-  site = relationship(site)
-
+  site = relationship(Site)
+  images = relationship('Image')
 
 
 class Image(Base):
@@ -63,16 +141,6 @@ class Image(Base):
   img = Column(LargeBinary())
   num = Column(Integer, nullable=False) # page number in the chapter
   chapter_id = Column(Integer, ForeignKey('chapter.id'))
+  type_mime = Column(String(50), nullable=False)
 
   chapter = relationship(Chapter)
-
-
-
-
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
-engine = create_engine('sqlite:///store.db')
-
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
-Base.metadata.create_all(engine)
