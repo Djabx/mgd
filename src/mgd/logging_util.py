6563367db986fa534d13a68a91f15b11891022c5
Alexandre Badez <alexandre.badez@gmail.com>
2014-12-28 13:11:47 +0100

    Ménage dans le module de logging.

--- src/mgd/logging_util.py
+++ src/mgd/logging_util.py
@@ -1,8 +1,6 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
 import logging
 import sys
 import inspect
@@ -66,103 +64,101 @@ LOGGING_CONF={
 
 
 def init_logger(conf_logging=LOGGING_CONF):
-    # now we init the logging module, this should be done only once
-    from logging import config
-    config.dictConfig(conf_logging)
+  # now we init the logging module, this should be done only once
+  from logging import config
+  config.dictConfig(conf_logging)
 
-    change_except_hook()
+  change_except_hook()
 
-    #logger.debug('logging init with: %s', formater.pformat(conf_logging))
+  #logger.debug('logging init with: %s', formater.pformat(conf_logging))
 
 
 
 def change_except_hook(length=2, local=True, code=True,
                        skipping=True):
-    """
-    Change the exception hook (how we handle uncaught exceptions)
-    The new hook, display local variables, and a bit more code
+  """
+  Change the exception hook (how we handle uncaught exceptions)
+  The new hook, display local variables, and a bit more code
 
-    :param length: how many lines of code we want to display before and after the bad line
-        if 0 display only the bad line
-    :param logger: the logger (see logging module) to use, if None, not use
-        in every case, the exception is also printed on sys.stderr
-    :param local: a boolean to set if we want to display local variable or not
-    :param skipping: a boolean to set if we want to skip some method (added with
-        add_except_name(name))
-    """
-    global _NUM_LINE_TO_SHOW, _DISPLAY_LOCAL, _SKIPPING, _DISPLAY_CODE
-    _NUM_LINE_TO_SHOW = length
-    _DISPLAY_LOCAL = local
-    _DISPLAY_CODE = code
-    _SKIPPING = skipping
-    sys.excepthook = _my_excepthook
-    logger.info('exception hook changed.')
+  :param length: how many lines of code we want to display before and after the bad line
+      if 0 display only the bad line
+  :param code: a boolean to set if we want to display code lines or not
+  :param local: a boolean to set if we want to display local variable or not
+  :param skipping: a boolean to set if we want to skip some method (added with
+      add_except_name(name))
+  """
+  global _NUM_LINE_TO_SHOW, _DISPLAY_LOCAL, _SKIPPING, _DISPLAY_CODE
+  _NUM_LINE_TO_SHOW = length
+  _DISPLAY_LOCAL = local
+  _DISPLAY_CODE = code
+  _SKIPPING = skipping
+  sys.excepthook = _my_excepthook
+  logger.info('exception hook changed.')
 
 
 
 def print_current_exception_information():
-    '''
-    Print the current exception stack trace
-    '''
-    _log_traceback(*sys.exc_info())
+  '''
+  Print the current exception stack trace
+  '''
+  _log_traceback(*sys.exc_info())
 
 
 
 def is_except_hook_changed():
-    """
-    Test if the except hook as been changed or not.
-    """
-    return sys.excepthook == _my_excepthook
+  """
+  Test if the except hook as been changed or not.
+  """
+  return sys.excepthook == _my_excepthook
 
 
 
 def add_except_name(name):
-    """
-    add the given name to excepted method
+  """
+  add the given name to excepted method
 
-    When an exception not catch will occur, if a method/function in the stack as
-    her name in the list, it will be skip.
-    """
-    _EXECEPTED_METHOD.append(name)
+  When an exception not catch will occur, if a method/function in the stack as
+  her name in the list, it will be skip.
+  """
+  _EXECEPTED_METHOD.append(name)
 
 
 ################################################################################
 # Private functions
 ################################################################################
-
 def _get_src_string(tb_frame):
-    """
-    Return a string for the given frame that represent his code section
+  """
+  Return a string for the given frame that represent his code section
 
-    Sometimes, code is illisible, or unfoundable
-    so we return an empty string and a false boolean (true oserwise)
-    """
-    bad_line_num = tb_frame.f_lineno
-    src_str = []
-    display = True
-    try:
-        src_, start_line = inspect.getsourcelines(
-            inspect.getmodule(tb_frame.f_code))
-    except TypeError:
-        display = False
-    else:
-        new_bad_line_num = bad_line_num - start_line
-        begin = max(new_bad_line_num - _NUM_LINE_TO_SHOW - 1, 0)
-        end = min(new_bad_line_num + _NUM_LINE_TO_SHOW, len(src_))
+  Sometimes, code is illisible, or unfoundable
+  so we return an empty string and a false boolean (true oserwise)
+  """
+  bad_line_num = tb_frame.f_lineno
+  src_str = []
+  display = True
+  try:
+    src_, start_line = inspect.getsourcelines(
+          inspect.getmodule(tb_frame.f_code))
+  except TypeError:
+    display = False
+  else:
+    new_bad_line_num = bad_line_num - start_line
+    begin = max(new_bad_line_num - _NUM_LINE_TO_SHOW - 1, 0)
+    end = min(new_bad_line_num + _NUM_LINE_TO_SHOW, len(src_))
 
-        for i, code_line in enumerate(src_[begin: end]):
-            line_num = i + begin + 1
-            src_str.append('%(indent)s%(num)04d%(sign)s%(code)s' %
-                    {'num' : line_num,
-                     'indent' : '%(indent_src)s',
-                    'sign' : '  ' if line_num != new_bad_line_num else '->',
-                    'code' : code_line.replace('%', '%%')})
+    for i, code_line in enumerate(src_[begin: end]):
+      line_num = i + begin + 1
+      src_str.append('{indent}{num:04}{sign}{code}'.format(
+            num=line_num,
+            indent='{indent_src}',
+            sign='  ' if line_num != new_bad_line_num else '->',
+            code=code_line))
 
-    return ''.join(src_str).rstrip(), display
+  return ''.join(src_str).rstrip(), display
 
 
 
-def _format_var(name, value, fmt="%20s = %s"):
+def _format_var(name, value, fmt='{:<40} = {}'):
     try:
         vs = formater.pformat(value).replace('%', '%%')
     except:
@@ -186,14 +182,14 @@ def _get_local_string(local_var):
 
         for k, v in sorted(self_var.__dict__.iteritems(),
                            cmp=lambda x, y: cmp(x[0], y[0])):
-            locals_.append(_format_var('self.' + k, v, "%40s = %s"))
+            locals_.append(_format_var('self.' + k, v, '{:<40} = {}'))
 
     for k, v in sorted(local_var.iteritems(),
                        cmp=lambda x, y: cmp(x[0], y[0])):
         locals_.append(_format_var(k, v))
 
     if len(locals_) == 0:
-        return '%20s' % 'None'
+        return '{:<20}'.format(None)
     return '\n'.join(locals_)
 
 
@@ -203,7 +199,7 @@ def _get_frame_string_list(frames, lresult):
     for frame in frames:
         if _SKIPPING and frame.f_code.co_name in _EXECEPTED_METHOD:
             # it's a methode/function name to skip
-            lresult.append('  skipping: %s' % frame.f_code.co_name)
+            lresult.append('  skipping: {}'.format(frame.f_code.co_name))
             continue
 
         # we get the source code
@@ -215,21 +211,21 @@ def _get_frame_string_list(frames, lresult):
              'locals':_get_local_string(frame.f_locals),
              'function':frame.f_code.co_name,
              'src':src_,
-             'indent':'%(indent)s',
-             'indent_src':'%(indent_src)s',
-             'mark':'%(mark)s',
+             'indent':'{indent}',
+             'indent_src':'{indent_src}',
+             'mark':'{mark}',
              }
         # the error location
-        lresult.append('''  File "%(file_)s", line %(line)d, in %(function)s''' % d)
+        lresult.append('  File "{file_}", line {line}, in {function}'.format(**d))
         if _DISPLAY_CODE and display_code:
             # we display the source code
-            lresult.append('%(indent)s%(indent)s%(mark)s Code %(mark)s' % d)
-            lresult.append('%(src)s' % d)
+            lresult.append('{indent}{indent}{mark} Code {mark}'.format(**d))
+            lresult.append('{src}'.format(**d))
 
         if _DISPLAY_LOCAL:
             # we display local variables
-            lresult.append('%(indent)s%(indent)s%(mark)s Locals %(mark)s' % d)
-            lresult.append('%(locals)s' % d)
+            lresult.append('{indent}{indent}{mark} Locals {mark}'.format(**d))
+            lresult.append('{locals}'.format(**d))
 
 
 
@@ -239,7 +235,7 @@ def _format_frame_string_list(lresult):
     d = {'indent':' ' * 4, 'indent_src':' ' * 8, 'mark':'#' * 5}
     # we gen he full error message
     error_msg = '\n'.join(lresult)
-    return error_msg % d
+    return error_msg.format(**d)
 
 
 
@@ -255,14 +251,14 @@ def _log_traceback(except_type, value, traceback):
     lresult = ['Traceback (most recent call last):']
     _get_frame_string_list(iter_tb_frame(traceback), lresult)
 
-    lresult.append('EXCEPTION : %s: %s' % (except_type.__name__, value))
+    lresult.append('EXCEPTION : {}: {}'.format(except_type.__name__, value))
 
     error_msg = _format_frame_string_list(lresult)
 
     if logger:
         # we use the logger
-        logger.debug('Exception occured and was not catched:\n%s', error_msg)
-        logger.critical('Exception occured and was not catched: %s: %s', except_type.__name__, value)
+        logger.debug('Exception occured and was not catched:\n{}'.format(error_msg))
+        logger.critical('Exception occured and was not catched: {}: {}'.format(except_type.__name__, value))
     else:
         # we write on stderr
         print(error_msg, file=sys.stderr)
