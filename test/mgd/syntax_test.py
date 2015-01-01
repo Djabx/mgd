85de863aae36f8fb4814d64822a28656a827bdf4
Alexandre Badez <alexandre.badez@gmail.com>
2015-01-01 19:22:27 +0100

    Ajout d'un simple test pour vérifier la syntax des fichiers. (niveau 0 du test...)

---
+++ test/mgd/syntax_test.py
@@ -0,0 +1,9 @@
+import mgd
+from mgd import _version
+from mgd import data_access
+from mgd import exceptions
+from mgd import logging_util
+from mgd import model
+from mgd import readers
+from mgd import writters
+from scripts import mgd
