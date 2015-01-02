b7831f45ff7fb9e8beaf0b084d783018205991f4
Alexandre Badez <alexandre.badez@gmail.com>
2015-01-02 18:06:23 +0100

    modification de l'initialisation.

--- src/scripts/mgd.py
+++ src/scripts/mgd.py
@@ -16,40 +16,56 @@ logger = logging.getLogger(__name__)
 from mgdpck import info
 from mgdpck.readers import *
 
+def _get_parser_sy(main_parser, default_store):
+  parser_sy = main_parser.add_parser('sy', help='command for syncing books in db')
+  parser_sy.set_defaults(func=handle_sy)
+
+  parser_sy.add_argument('-d', '--data',
+    dest='data_store',
+    help='the output where to store all data (default to: {})'.format(default_store),
+    default=default_store)
+
+  group = parser_sy.add_mutually_exclusive_group(required=True)
+  group.add_argument('--nothing',
+    action='store_true',
+    help='Does nothing')
+  group.add_argument('--all',
+    action='store_true',
+    help='Do all sync operations')
+  group.add_argument('--site',
+    action='store_true',
+    help='Sync all known sites')
+  group.add_argument('--books',
+    action='store_true',
+    help='Sync all books')
+  group.add_argument('--chapter',
+    action='store_true',
+    help='Sync chapters for books mark has followed')
+  group.add_argument('--content',
+    action='store_true',
+    help='Sync content structure (not images!) for books mark has followed')
+
+
 def get_parser():
   main_parser = argparse.ArgumentParser(prog='mgd', conflict_handler='resolve')
 
   cmd_parser = main_parser.add_subparsers(help='sub-command help')
   default_store = join('.', 'mgd_store.db')
 
-  parser_sy = cmd_parser.add_parser('sy', help='cmd for sync comics in db')
-  parser_sy.set_defaults(func=handle_sy)
-  parser_sy.add_argument('-d', '--data',
-    dest='data_store',
-    help='the output where to store all data (default to: %s)' % default_store,
-    default=default_store)
-  group = parser_sy.add_mutually_exclusive_group()
-  group.add_argument('--all',
-    dest='site_2_sync',
-    action='store_const', const='all',
-    help='Sync all known sites')
-  group.add_argument('-s', '--site',
-    dest='site_2_sync',
-    action='append',
-    help='the site hostname to sync')
+  _get_parser_sy(cmd_parser, default_store)
 
-  parser_dl = cmd_parser.add_parser('dl', help='cmd for dl comics in db')
+  parser_dl = cmd_parser.add_parser('dl', help='command for download comics in datastore')
   parser_dl.set_defaults(func=handle_dl)
   parser_dl.add_argument('-d', '--data',
     dest='data_store',
-    help='the output where to store all data (default to: %s)' % default_store,
+    help='the output where to store all data (default to: {})'.format(default_store),
     default=default_store)
 
-  parser_out = cmd_parser.add_parser('out', help='cmd for writting comics too ouput')
+  parser_out = cmd_parser.add_parser('out', help='command for writting comics to ouput')
   parser_out.set_defaults(func=handle_out)
   parser_out.add_argument('-d', '--data',
     dest='data_store',
-    help='the output where to store all data (default to: %s)' % default_store,
+    help='the output where to store all data (default to: {})'.format(default_store),
     default=default_store)
 
   return main_parser
@@ -65,21 +81,35 @@ def init_data_store(args):
   model.create_db(args.data_store)
 
 
-def handle_sy(args):
+def handle_sy(parser, args):
   logger.debug('sy cmd')
-  if args.site_2_sync == 'all':
-    logger.info('update all site')
-    args.site_2_sync = info.update_books_all_site()
+  logger.debug('args: %s', args)
+  init_data_store(args)
+  if not args.nothing:
+    if args.all or args.site:
+      logger.info('update all site')
+      info.create_all_site()
+
+    if args.all or args.books:
+      logger.info('update all books')
+      info.update_books_all_site()
 
 
-def handle_dl(args):
+def handle_dl(parser, args):
   logger.debug('dl cmd')
+  init_data_store(args)
   pass
 
 
-def handle_out(args):
+def handle_out(parser, args):
   logger.debug('out cmd')
+  init_data_store(args)
   pass
+
+
+def handle_default(parser, args):
+  logger.debug('out default')
+  parser.print_help()
 
 
 
@@ -89,10 +119,10 @@ def main():
   args = parser.parse_args()
   logger.debug('Parsing arguments: %s', args)
 
-  init_data_store(args)
+  if not hasattr(args, 'func'):
+    args.func = handle_default
 
-  info.create_all_site()
-  args.func(args)
+  args.func(parser, args)
 
 
 if __name__ == '__main__':
